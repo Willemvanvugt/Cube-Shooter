f91e721f3d2a111457cf922538e31570acbbc6d1
Willemvanvugt <148279034+Willemvanvugt@users.noreply.github.com>
2024-04-08 21:20:27 +0200

    Commit #2: in-game menu
    Added a Resume and Exit button.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,10 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject menuCanvas; //Game object containing the Resume and Menu buttons.
+    public KeyCode menuButton; //Key for displaying the menu.
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(menuButton))
+        {
+            PauseGame();
+        }
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
         Debug.Log("Scene 1 was loaded.");
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("Scene 0 was loaded.");
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0;
+        menuCanvas.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        menuCanvas.SetActive(false);
     }
 
     public void ExitGame()
